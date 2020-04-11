2065bf0514ec29a141401dd1b6616c7d664b1a8e
Simon Burton <simon@arrowtheory.com>
2020-04-11 19:32:24 +0100

    commit edits

--- render.py
+++ render.py
@@ -9,9 +9,10 @@ seed(0)
 
 from pyx import canvas, path, deco, trafo, style, text, color, deformer
 from pyx.color import rgb
+from pyx.bbox import bbox
 rgbhex = color.rgbfromhexstring
 
-from turtle import Turtle
+from turtle import Turtle, dopath
 
 
 #text.set(mode="latex") 
@@ -103,14 +104,44 @@ def conv(x0, y0, x1, y1, a):
 def arrow(x0, y0, x1, y1, a=0.8, extra=[]):
     x2, y2 = conv(x0, y0, x1, y1, a)
     cvs.stroke(path.line(x0, y0, x2, y2), [deco.earrow()]+st_thick+extra)
-    cvs.stroke(path.line(x0, y0, x1, y1), st_thick+extra)
+    if a < 1.0:
+        cvs.stroke(path.line(x0, y0, x1, y1), st_thick+extra)
 
 
-def draw_poset(chain, flow):
+def hitlist(x0, y0, x1, y1):
+    bb0 = bbox(x0, y0, x1, y1)
+    hits = []
+    for item in cvs:
+        bb1 = item.bbox()
+        if bb0.intersects(bb1):
+            hits.append(item)
+    return hits
 
-    dx = 1.0
-    dy = 2.0
-    r = 0.06
+
+def annotate(x, y, r, label): 
+    count = None
+    best = None
+    epsilon = 0.1*r
+    #debug = print if label=="$v_{2}$" else lambda *args,**kw:None
+    #debug(label)
+    for (dx, dy) in [
+        ( 1,  0), (-1,  0), ( 0,  1), ( 0, -1)]:
+        x1 = x+r*dx
+        y1 = y+r*dy
+        #hits = hitlist(x1-r, y1-r, x1+r, y1+r)
+        hits = hitlist(x1-epsilon, y1-epsilon, x1+epsilon, y1+epsilon)
+        #debug("\t", dx, dy)
+        #for hit in hits:
+            #debug("\t", hit)
+        if count is None or len(hits) < count:
+            count = len(hits)
+            best = x1, y1
+    x1, y1 = best
+    #debug("count:", count, label)
+    cvs.text(x1, y1, label, center)
+
+
+def draw_poset(chain, flow, dx=1.0, dy=2.0, r=0.06):
 
     y = 0.
     layout = {} # map Cell -> (x,y)
@@ -132,7 +163,7 @@ def draw_poset(chain, flow):
     for grade in grades:
         y = grade * dy
         cvs.stroke(path.line(xmin, y, xmax, y), [darkgrey])
-        cvs.text(xmin-3*r, y, r"$X_%d$"%grade, east)
+        cvs.text(xmin-5*r, y, r"$X_%d$"%grade, east)
     
     #grade = 1
     for grade in grades:
@@ -164,44 +195,110 @@ def draw_poset(chain, flow):
             cvs.stroke(path.circle(x, y, 2*r), [red]+st_thick)
         cvs.fill(path.circle(x, y, r))
 
+        name = "$%s$"%cell.name
+        if cell.grade == 0:
+            cvs.text(x, y-3*r, name, north)
 
-def draw_complex(chain, flow):
+        elif cell.grade == grades[-1]:
+            cvs.text(x, y+3*r, name, south)
+
+        else:
+            cvs.text(x+3*r, y-r, name, northwest)
+
+
+def draw_complex(chain, flow, scale):
 
     r = 0.07
 
     critical = flow.get_critical()
-    A = chain.get_bdymap(1) # 1 --> 0
+    A1 = chain.get_bdymap(1) # 1 --> 0
+    A2 = chain.get_bdymap(2) # 2 --> 1
     verts = chain.get_cells(0)
     edges = chain.get_cells(1)
-    matches = flow.get_pairs(0)
+    faces = chain.get_cells(2)
 
-    # Edges
+    bdys = {} # map cell --> list of bdy cells
+
+    for f in faces:
+        bdy = []
+        for e in edges:
+            if A2[e, f]:
+                bdy.append(e)
+        bdys[f] = bdy
+
     for e in edges:
         bdy = []
         for v in verts:
-            if A[v, e]:
+            if A1[v, e]:
                 bdy.append(v)
+        bdys[e] = bdy
+
+    for e in edges:
+        bdy = bdys[e]
         if len(bdy) != 2:
             continue
+        v0, v1 = bdy
+        x0, y0 = v0.pos
+        x1, y1 = v1.pos
+        x2, y2 = conv(x0, y0, x1, y1, 0.5)
+        e.pos = x2, y2
 
+    # Draw Faces
+    matches = flow.get_pairs(1)
+    for f in faces:
+        bdy = bdys[f]
+        assert bdy
+
+        hull = []
+        for e in bdy:
+            hull += bdys[e]
+        hull = list(set(hull)) # uniq
+
+        x0 = sum([v.pos[0] for v in hull]) / len(hull)
+        y0 = sum([v.pos[1] for v in hull]) / len(hull)
+
+        if f.pos == None:
+            f.pos = x0, y0
+
+        if not f.infty:
+            hull = [v.pos for v in hull]
+            hull = [conv(x, y, x0, y0, 0.2) for (x,y) in hull]
+            #p = path.path(path.moveto(*hull[0]),
+            #cvs.fill(p, [grey])
+            dopath(hull, fill=[grey], stroke=False)
+
+        for e in bdy:
+            if (e, f) in matches:
+                x1, y1 = e.pos
+                if f.infty:
+                    x0 = 2*x1
+                    y0 = 2*y1
+                    f.pos = x0, y0
+                arrow(x1, y1, x0, y0, 1.0, st_THick+[orange])
+
+        if f in critical:
+            cvs.stroke(path.circle(f.pos[0], f.pos[1], r), [red]+st_thick)
+
+    # Draw Edges
+    matches = flow.get_pairs(0)
+    for e in edges:
+        bdy = bdys[e]
         v0, v1 = bdy
         if (v1, e) in matches:
             v0, v1 = v1, v0 # swap
         x0, y0 = v0.pos
         x1, y1 = v1.pos
+        x2, y2 = e.pos
 
-        x2, y2 = conv(x0, y0, x1, y1, 0.5)
         if (v0, e) in matches:
-            arrow(x0, y0, x2, y2, 1.1, st_THick+[orange])
+            arrow(x0, y0, x2, y2, 1.0, st_THick+[orange])
 
-        else:
-            assert e in critical
-            #cvs.stroke(path.line(x0, y0, x1, y1), st_THick+[red])
+        elif e in critical:
             cvs.stroke(path.circle(x2, y2, r), [red]+st_thick)
 
         cvs.stroke(path.line(x0, y0, x1, y1))
 
-    # Vertices
+    # Draw Vertices
     for cell in verts:
         x, y = cell.pos
     
@@ -210,12 +307,11 @@ def draw_complex(chain, flow):
             cvs.stroke(path.circle(x, y, 2*r), [red]+st_thick)
         cvs.fill(path.circle(x, y, r))
 
+        name = "$%s$"%cell.name
+        annotate(x, y, 5*r, name)
 
 
 # ---------------------------------------------------------
-
-dx = 2.0
-dy = 2.0
 
 M = parse("""
 11.....
@@ -230,24 +326,48 @@ ring = element.FiniteField(2)
 
 #rows = "1 2 3 4 5 6".split()
 #cols = "1 2 3 4 5 6 7".split()
+chains = []
+
 chain = Chain.fromnumpy(M, ring)
 verts = chain.cells[0]
 for i, v in enumerate(verts):
     x = (i//2)
     y = -(i%2)
-    v.pos = (dx*x, dy*y)
+    v.pos = x, y
+chains.append(chain)
 
+chain = Assembly.build_tetrahedron(ring).get_chain()
+chains.append(chain)
 
-#chain = Assembly.build_tetrahedron(ring).get_chain()
-#chain = Assembly.build_torus(2, 2, ring).get_chain()
-flow = Flow(chain)
-flow.build()
+chain = Assembly.build_torus(2, 2, ring).get_chain()
+chains.append(chain)
 
+scale = 2.0
+for chain in chains:
+    for cell in chain.get_cells():
+        if cell.pos is None:
+            continue
+        x, y = cell.pos
+        cell.pos = scale*x, scale*y
 
-cvs = canvas.canvas()
-#draw_poset(chain, flow)
-draw_complex(chain, flow)
-save("pic-poset-1")
+for idx, chain in enumerate(chains):
+
+#    if idx != 0:
+#        continue
+
+    flow = Flow(chain)
+    flow.build()
+    
+    cvs = canvas.canvas()
+    draw_complex(chain, flow, scale)
+
+    save("pic-complex-%d"%idx)
+
+    #break
+    
+    cvs = canvas.canvas()
+    draw_poset(chain, flow)
+    save("pic-poset-%d"%idx)
 
 
 
